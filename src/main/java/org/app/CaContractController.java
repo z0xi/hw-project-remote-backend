173ca146184a0e63869f2d4341469cb808931508
發 <55946235+fajin0@users.noreply.github.com>
2022-06-19 13:14:37 +0800

    Update CaContractController.java

--- src/main/java/org/app/CaContractController.java
+++ src/main/java/org/app/CaContractController.java
@@ -1,12 +1,11 @@
 package org.app;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Maps;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.codec.binary.StringUtils;
-import org.hyperledger.fabric.client.Contract;
-import org.hyperledger.fabric.client.Gateway;
-import org.hyperledger.fabric.client.GatewayException;
+import org.hyperledger.fabric.client.*;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -29,6 +32,7 @@ public class CaContractController {
     final Contract contract;
 
     final HyperLedgerFabricProperties hyperLedgerFabricProperties;
+    public static int count = 1;
 
 
     @GetMapping("/{key}")
@@ -60,7 +64,7 @@ public class CaContractController {
         Map<String, Object> result = Maps.newConcurrentMap();
 
         contract.newProposal("createCa")
-                .addArguments(ca.getKey(), ca.getKey(), ca.getName(), ca.getAge(), ca.getGrade(), ca.getSubject(), ca.getUniversity(), ca.getHashAlgorithm(), ca.getIssure(), ca.getSignature(), ca.getSignatureAlgorithm())
+                .addArguments(ca.getKey(), ca.getName(), ca.getAge(), ca.getGrade(), ca.getSubject(), ca.getUniversity(), ca.getHashAlgorithm(), ca.getIssure(), ca.getSignature(), ca.getSignatureAlgorithm())
                 .build()
                 .endorse()
                 .submitAsync();
@@ -93,6 +97,77 @@ public class CaContractController {
         result.put("status", "ok");
 
         return result;
+    }
+
+    @GetMapping("/queryAll")
+    public Map<String, Object> queryAll() throws GatewayException {
+
+        Map<String, Object> result = Maps.newConcurrentMap();
+        byte[] ca = contract.evaluateTransaction("queryCaAll");
+
+        result.put("payload", StringUtils.newStringUtf8(ca));
+        result.put("status", "ok");
+
+        return result;
+    }
+
+    @PutMapping("/upload")
+    public Map<String, Object> uploadByName(@RequestBody String name) throws IOException {
+        Map<String, Object> result = Maps.newConcurrentMap();
+        String key = "ca-" + count;
+        String age = encodeFile("./server_folder/age");
+        String grade = encodeFile("./server_folder/grade");
+        String subject = encodeFile("./server_folder/subject");
+        String university = encodeFile("./server_folder/university");
+        String hashAlgorithm = encodeFile("./server_folder/hashAlgorithm");
+        String issure = encodeFile("./server_folder/issure");
+        String signature = encodeFile("./server_folder/signature");
+        String signatureAlgorithm = encodeFile("./server_folder/signatureAlgorithm");
+        try {
+            contract.newProposal("createCa")
+                    .addArguments(key, name, age, grade, subject, university, hashAlgorithm, issure, signature, signatureAlgorithm)
+                    .build()
+                    .endorse()
+                    .submitAsync();
+        } catch (SubmitException e) {
+            System.out.println("交易提交失败！");
+        } catch (EndorseException e) {
+            System.out.println("交易超时！请重新提交");
+            throw new RuntimeException(e);
+        }
+        result.put("status", "ok");
+        return result;
+    }
+
+    @PutMapping("/verify")
+    public Map<String, Object> verifyProperties(@RequestBody String[] properties) throws IOException {
+        Map<String, Object> result = Maps.newConcurrentMap();
+        for (String property:properties) {
+            byte[] readFileToByteArray = FileUtils.readFileToByteArray(new File("./server_folder/" + property));
+            String codes = readFileToByteArray.toString();
+            decodeFile(codes, "./verifier_folder/" + property);
+        }
+        result.put("status", "ok");
+        return result;
+    }
+
+
+    public static String encodeFile(File file) throws IOException {
+        byte[] readFileToByteArray = FileUtils.readFileToByteArray(file);
+        return Base64.encodeBase64String(readFileToByteArray);
+    }
+
+    public static String encodeFile(String filePath) throws IOException {
+        return encodeFile(new File(filePath));
+    }
+
+    public static void decodeFile(String codes, File file) throws IOException {
+        byte[] decodeBase64 = Base64.decodeBase64(codes);
+        FileUtils.writeByteArrayToFile(file, decodeBase64);
+    }
+
+    public static void decodeFile(String codes, String filePath) throws IOException {
+        decodeFile(codes, new File(filePath));
     }
 
 }
